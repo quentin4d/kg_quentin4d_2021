ec1da842e4ecb681479dd3d4cd3e178b2df28285
quentin4d <quentinford@Quentins-MacBook-Pro.local>
2021-05-02 23:48:50 -0500

    Added descriptive method header

--- main.js
+++ main.js
@@ -7,6 +7,17 @@ const inputNumbers = process.argv.slice(2);
 //Verify that none of the integer args are negative
 console.log( inputNumbers.find(elem => elem < 0) ? "All integers must be positive!" :  convertToStrings(inputNumbers) );
 
+/**
+ * Given an array containing positive integers, prints out the strings 
+ * representing the phonetic equivalent of each integer
+ * 
+ * Ex: 
+ *  Given an array: [3, 25, 209], print “Three,TwoFive,TwoZeroNine” into stdout. 
+ *  Given an array: [10, 300, 5], print “OneZero,ThreeZeroZero,Five” into stdout.
+ * 
+ * @param arr - Array containing all integers to be converted to strings
+ * @returns - String containing all (comma-separated) string representations of the values of arr
+ */
 function convertToStrings(arr) {
 
     let outputString = "";
