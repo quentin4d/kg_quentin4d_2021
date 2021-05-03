8c699882212986592bf360f1f7d7cce9bcafce13
quentin4d <quentinford@Quentins-MacBook-Pro.local>
2021-05-02 23:25:12 -0500

    Accept user input arguments

--- main.js
+++ main.js
@@ -1,19 +1,21 @@
-const numbers = ["Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"]
+//0-9 in plain english strings
+const digits = ["Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"];
+const inputNumbers = process.argv.slice(2);
 
-//should pring out "OneZero,ThreeZeroZero,Five"
-console.log(convert([10,300,5]));
+console.log(convertToStrings(inputNumbers));
 
-function convert(arr) {
+function convertToStrings(arr) {
     let outputString = "";
     //iterate over strings in the array
-    arr.forEach((digit, idx) => {
-        let digString = digit.toString();
+    arr.forEach((number, idx) => {
+        let numberString = number.toString();
         //iterate over characters in each string
-        [...digString].forEach(char => {
-            outputString += numbers[parseInt(char)]
+        [...numberString].forEach(char => {
+            outputString += digits[parseInt(char)]
         })
         //add comma between every array number, excluding last one
         outputString += (idx < arr.length-1) ? "," : "";
     });
+
     return outputString;
 }
